0b49fee9ade45a961607e1d084432bccfbe592a5
Celina Lopez <marisa.celina.lopez4@gmail.com>
2023-10-15 13:37:56 -0700

    list em

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, send_from_directory, render_template, redirect, url_for
 from app.main import startup_company, feed_back
+import os
 
 appz = Flask(__name__)
 
@@ -12,6 +13,13 @@ def create():
 
         return redirect(url_for('update', sha_id=sha))
     elif request.method == 'GET':
+        available_shas = os.listdir('examples/games')
+        return render_template('list.html', shas=available_shas)
+
+
+@appz.route('/new', methods=['GET'])
+def create():
+    if request.method == 'GET':
         return render_template('home.html')
 
 
