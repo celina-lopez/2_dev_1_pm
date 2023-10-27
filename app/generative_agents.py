b8dc9fc7e174d9a1e3935f97889e3e87687890aa
Celina Lopez <marisa.celina.lopez4@gmail.com>
2023-10-27 12:01:09 -0700

    rewrite functions

--- app/generative_agents.py
+++ app/generative_agents.py
@@ -5,103 +5,83 @@ from dotenv import load_dotenv
 load_dotenv(dotenv_path='../.env')
 openai.api_key = os.getenv("OPENAI_API_KEY")
 MODEL = 'gpt-3.5-turbo'
-
-
-def create_project_manager(ask):
-    messages = [
-        {
-            "role": "system",
-            "content": """You are a project manager at a gaming startup company.
+PERSONAS = {
+    "project_manager": {
+        "system": """You are a project manager at a gaming startup company.
       Your goal is to design a simple game in javascript.
       You just met with the CEO who said they want to build this game: {}.
       You are meeting with the an Enginner.  
       You will need give an outline and description of the basic concepts and gameplay mechanics of the game in order for the Engineer to build it.
-      """.format(ask)
-        },
-        {
-            "role": "user",
-            "content": "What is the game about? Could you give me a outline and description? What are the basic concepts and gameplay mechanics?"
-        },
+      """,
+        "user": "What is the game about? Could you give me a outline and description? What are the basic concepts and gameplay mechanics?"
+    },
+    "game_designer": {
+        "system": """
+        Your goal is to design a simple game in javascript.
+        You work for a gaming startup company.
+        You are talking to a Project Manger who wants you to build this: {}.
+        """,
+        "user": "Give back only the HTML with all the code within including Javascript and CSS."
+    },
+    'feedback_pm': {
+        'system': """
+        You are a Project Manager at a gaming startup company. The CEO has asked you to build a game called {}.
+        The CEO gave you this feedback: {}.
+        You are talking to the Enginner.
+        """,
+        'user': "What is the feedback?"
+    },
+    'feedback_eng': {
+        'system': """
+        You need to correct the game designer's mistakes. I am a Project Manager that is giving you feedback.
+        Here is the HTML code for the game: {}. Here is also the project timeline/description: {}
+        Give back only the HTML with all the code within including javascript and css.
+        """,
+    },
+    'designer': {
+        'system': """
+        You are a designer at a gaming startup company.
+        Your goal is to design the cover art for this new game: {}.
+        You inputting a Dalle-3 Image Generation prompt that will generate the cover art for the game.
+        ONLY GIVE THE DALLE-3 PROMPT.
+        Prompt must be length 1000 or less.
+        """,
+        'user': "Give back the Dalle-3 prompt for the cover art for the game. ONLY GIVE THE DALLE-3 PROMPT"
+    }
+}
+
+
+def message_to_openai(persona, *args):
+    return [
+        {"role": "system",
+            "content": PERSONAS[persona]['system'].format(*args)},
+        {"role": "user",
+            "content": PERSONAS[persona]['user']},
     ]
+
+
+def openai_content(messages):
     response = openai.ChatCompletion.create(model=MODEL, messages=messages)
     return response.choices[0].message.content
 
 
-def create_game_designer(ask, pm_message):
-    messages = [
-        {
-            "role": "system",
-            "content": """
-      Your goal is to design a simple game in javascript.
-      You work for a gaming startup company.
-      You are talking to a Project Manger who wants you to build this: {}.
-      """.format(ask, pm_message)
-        },
-        {
-            "role": "user",
-            "content": "Give back only the HTML with all the code within including Javascript and CSS."
-        },
-    ]
-    response = openai.ChatCompletion.create(model=MODEL, messages=messages)
-    return response.choices[0].message.content
-
-
-def feed_back_pm(feedback, ask):
-    messages = [
-        {
-            "role": "system",
-            "content": """
-            You are a Project Manager at a gaming startup company. The CEO has asked you to build a game called {}.
-            The CEO gave you this feedback: {}.
-            You are talking to the Enginner.
-      """.format(ask, feedback)
-        },
-        {
-            "role": "user",
-            "content": "What is the feedback?"
-        },
-    ]
-    response = openai.ChatCompletion.create(model=MODEL, messages=messages)
-    return response.choices[0].message.content
+def create_persona(role, *args):
+    messages = message_to_openai(role, *args)
+    return openai_content(messages)
 
 
 def feed_back_eng(html_code, project_description, pm_feedback):
     messages = [
         {
             "role": "system",
-            "content": """
-        You need to correct the game designer's mistakes. I am a Project Manager that is giving you feedback.
-        Here is the HTML code for the game: {}. Here is also the project timeline/description: {}
-      Give back only the HTML with all the code within including javascript and css.
-      """.format(html_code, project_description)
+            "content": PERSONAS['feedback_eng']['system'].format(html_code, project_description)
         },
         {
             "role": "user",
             "content": pm_feedback
         },
     ]
-    response = openai.ChatCompletion.create(model=MODEL, messages=messages)
-    return response.choices[0].message.content
-
-
-def create_designer(ask):
-    messages = [
-        {
-            "role": "system",
-            "content": """You are a designer at a gaming startup company.
-      Your goal is to design the cover art for this new game: {}.
-      You inputting a Dalle-3 Image Generation prompt that will generate the cover art for the game.
-    ONLY GIVE THE DALLE-3 PROMPT.
-    Prompt must be length 1000 or less.
-      """.format(ask)
-        },
-        {
-            "role": "user",
-            "content": "Give back the Dalle-3 prompt for the cover art for the game. ONLY GIVE THE DALLE-3 PROMPT"
-        },
-    ]
-    response = openai.ChatCompletion.create(model=MODEL, messages=messages)
-    return response.choices[0].message.content
+    return openai_content(messages)
 
 
 def dalle_3_designer(pm_message):
--- app/main.py
+++ app/main.py
@@ -2,40 +2,28 @@ import openai
 import os
 from dotenv import load_dotenv
 import json
-import pdb
 from app.utils import save_project, parse_html
-from app.generative_agents import create_game_designer, create_project_manager, feed_back_pm, feed_back_eng, create_designer, dalle_3_designer
+from app.generative_agents import create_persona, feed_back_eng, dalle_3_designer
 
 load_dotenv(dotenv_path='../.env')
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 MODEL = 'gpt-3.5-turbo'
 
-ASK = 'flappy bird'  # Use for create_project_manager
-
-# TODO:  write history!
-
 
 def startup_company(ask, project_name=''):
-    pm_message = create_project_manager(ask)
-    html_message = create_game_designer(ask, pm_message)
-    html_code = parse_html(html_message)
-    designer_message = create_designer(ask)
+    pm_message = create_persona('project_manager', ask)
+    html_message = create_persona('game_designer', ask, pm_message)
+    html = parse_html(html_message)
+    designer_message = create_persona('game_designer', ask)
     image_url = dalle_3_designer(designer_message)
     history = [{
         'ask': ask,
         "pm_message": pm_message,
         "html_message": html_message,
     }]
-    sha = save_project(html_code, history, project_name, image_url)
-    return sha
-
-
-def sample_function(sha):
-    user_input = input("Enter a list of values separated by commas: ")
-    user_list = user_input.split(',')
-    user_list = [item.strip() for item in user_list]
-    feed_back(user_list, sha)
+    uuid = save_project(html, history, project_name, image_url)
+    return uuid
 
 
 def feed_back(feedback, sha):
@@ -44,8 +32,7 @@ def feed_back(feedback, sha):
     ask = history[-1]['ask']
     with open('./examples/games/{}/home.html'.format(sha), 'r') as outfile:
         html_code = outfile.read()
-    feedback_message = feed_back_pm(
-        "\n".join(feedback), ask)  # feed back is a list
+    feedback_message = create_persona('feedback_pm', "\n".join(feedback), ask)
     feedback_html_code = feed_back_eng(
         feedback_message, history[0]['pm_message'], html_code)
     new_html_code = parse_html(feedback_html_code)
@@ -53,14 +40,7 @@ def feed_back(feedback, sha):
         'ask': ",".join(feedback_message),
         "pm_message": feedback_message,
         "html_message": feedback_html_code,
-        "prevous_sha": sha,
     }
     history.append(new_history)
-    sha = save_project(new_html_code, history)
+    sha = save_project(new_html_code, history)  # TODO: update project
     return sha
-# pdb.set_trace()
-
-# app.main.sample_function("20231014185300")
-# the game is too fast could we slow it down, the game over should not be a pop up but displayed over the canvas, make the background of the the game light blue
-
-# startup_company(ASK)
